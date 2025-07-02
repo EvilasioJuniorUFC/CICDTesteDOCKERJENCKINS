098b1d143ba8a0ec236463cb823e56785d577c4e
EvilasioJuniorUFC <evilasiojunior@ufc.br>
2025-07-02 20:11:20 -0300

    cid project

---
+++ CICDTest/src/main/java/com/mycompany/cicdtest/CICDTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cicdtest;
+
+/**
+ *
+ * @author Steam Deck
+ */
+public class CICDTest {
+
+    public static void main(String[] args) {
+        System.out.println("====================================");
+        System.out.println("Aplicação Java rodando no Docker!");
+        System.out.println("Versão Java: " + System.getProperty("java.version"));
+        System.out.println("====================================");
+    }
+}
